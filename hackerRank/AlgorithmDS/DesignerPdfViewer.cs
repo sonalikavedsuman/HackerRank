559aa4b7482464d1795eac44c8b4a6d8c25f8dbc
Sonalika <sonalika011@gmail.com>
2023-10-17 09:30:38 +0530

    algorithm

---
+++ hackerRank/AlgorithmDS/DesignerPdfViewer.cs
@@ -0,0 +1,65 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'designerPdfViewer' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY h
+     *  2. STRING word
+     */
+
+    public static int designerPdfViewer(List<int> h, string word)
+    {
+        
+        int wordLength = word.Length;
+        int maxHeight = 0;// Initialize maxHeight to a minimum 
+        string alphabets = "abcdefghijklmnopqrstuvwxyz";
+        
+        foreach (char letter in word)
+        {
+            int index = alphabets.IndexOf(letter); 
+            int height = h[index];  
+            if (height > maxHeight)
+            {
+               maxHeight = height; 
+            }
+        }
+        return wordLength * maxHeight;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        List<int> h = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(hTemp => Convert.ToInt32(hTemp)).ToList();
+
+        string word = Console.ReadLine();
+
+        int result = Result.designerPdfViewer(h, word);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
