e874785af60991838cae6fa07c16de29a85a30a0
Sonalika <sonalika011@gmail.com>
2023-10-19 08:45:26 +0530

    algorithm

---
+++ hackerRank/AlgorithmDS/BeautifulDaysAtMovies.cs
@@ -0,0 +1,73 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'beautifulDays' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER i
+     *  2. INTEGER j
+     *  3. INTEGER k
+     */
+
+    public static int beautifulDays(int i, int j, int k)
+    {
+        int count = 0;
+        while(i <= j) 
+        { 
+          //Console.WriteLine(i);
+           string iStr = Convert.ToString(i);
+           string iReversed = new string(iStr.Reverse().ToArray());
+           //Console.WriteLine(iReversed);
+
+           int intI = Convert.ToInt32(i);
+           int intIRev = Convert.ToInt32(iReversed);
+           int Result = Math.Abs(intIRev - intI);
+           if(Result % k == 0 )
+           {
+                   count++;
+           }
+           i++;
+        }
+        return(count);
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int i = Convert.ToInt32(firstMultipleInput[0]);
+
+        int j = Convert.ToInt32(firstMultipleInput[1]);
+
+        int k = Convert.ToInt32(firstMultipleInput[2]);
+
+        int result = Result.beautifulDays(i, j, k);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
---
+++ hackerRank/AlgorithmDS/SherlockAndSquares.cs
@@ -0,0 +1,72 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'squares' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER a
+     *  2. INTEGER b
+     */
+
+    public static int squares(int a, int b)
+    {
+      int count = 0;
+      int n = 1;
+      int square = n * n;
+      
+      while (square <= b)
+      {
+         if (square >= a)
+         {
+             count++;
+         }
+      
+         n++;
+         square = n * n;
+      }
+      return (count);
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int q = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int qItr = 0; qItr < q; qItr++)
+        {
+            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+            int a = Convert.ToInt32(firstMultipleInput[0]);
+
+            int b = Convert.ToInt32(firstMultipleInput[1]);
+
+            int result = Result.squares(a, b);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
