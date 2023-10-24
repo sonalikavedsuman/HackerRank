cce791261d02735e8382fd2fd02864887f468ab3
Sonalika <sonalika011@gmail.com>
2023-10-24 09:56:39 +0530

    algorithm

---
+++ hackerRank/AlgorithmDS/CountingValleys.cs
@@ -0,0 +1,70 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'countingValleys' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER steps
+     *  2. STRING path
+     */
+
+    public static int countingValleys(int steps, string path)
+    {
+        int seaLevel = 0; 
+        int currentAltitude = 0;
+        int valleyCount = 0;
+
+        foreach (char step in path)
+        {
+            if (step == 'U')
+            {
+               currentAltitude++; 
+            }
+            else if (step == 'D')
+            {
+               currentAltitude--; 
+            }
+            if (currentAltitude == 0 && step == 'U')
+            {
+               valleyCount++;
+            }
+        }
+        return valleyCount;
+    }
+    
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int steps = Convert.ToInt32(Console.ReadLine().Trim());
+
+        string path = Console.ReadLine();
+
+        int result = Result.countingValleys(steps, path);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
